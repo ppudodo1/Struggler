3d70d3777a3fc812c170a19c7b457524c9e2f609
ppudodo <dkatavic03@gmail.com>
2024-10-24 21:14:10 +0200

    jumping added

--- Struggler/Assets/Scripts/PlayerMovement.cs
+++ Struggler/Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,41 @@ using UnityEngine;
 
 public class PlayerMovement:MonoBehaviour
 {
-    public float speed;
-    public float jump;
-    public float Move;
-    public Rigidbody2D rb;
+    private float horizontal;
+    private float speed = 5f;
+    public float jumpingPower = 4f;
+    private bool isFacingRight = true;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
      void Start()
     {
       
     }
      void Update()
     {
-        Move = Input.GetAxis("Horizontal");
-        rb.linearVelocity = new Vector2(speed * Move, rb.linearVelocity.y);
+        horizontal = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump") && IsGrounded()) {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+            
+        }
+        Flip();
+    }
+    // treba rijesiti double jump sada
+    private bool IsGrounded() {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+    }
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f) {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 }
