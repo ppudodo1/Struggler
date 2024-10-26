646b9b4ceb0fec4248c98cab322f342ec9e272a8
ppudodo <dkatavic03@gmail.com>
2024-10-26 20:28:05 +0200

    Added sword

--- Struggler/Assets/Scripts/CameraFollow.cs
+++ Struggler/Assets/Scripts/CameraFollow.cs
@@ -3,12 +3,9 @@ using UnityEngine;
 public class CameraFollow:MonoBehaviour
 {
     public Transform player;
-    public float smoothSpeed = 0.125f;
     public Vector3 offset;
-    private void Update()
+     void Update()
     {
-        Vector3 desiredPosition = player.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
     }
 }
