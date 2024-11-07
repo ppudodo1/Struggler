1a83e452a22df3defaf19971ee8dc871dfa98cf9
glavatazelva <72494135+glavatazelva@users.noreply.github.com>
2024-11-07 18:33:41 +0100

    Scale Update

--- Struggler/Assets/Scripts/CameraFollow.cs
+++ Struggler/Assets/Scripts/CameraFollow.cs
@@ -6,6 +6,6 @@ public class CameraFollow:MonoBehaviour
     public Vector3 offset;
      void Update()
     {
-        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
+        transform.position = new Vector3(player.position.x + offset.x,player.position.y - 0.5f + offset.y, offset.z);
     }
 }
