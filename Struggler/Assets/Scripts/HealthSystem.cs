3428ba65416ccc163cb7f7f4c182e6b1e393115b
glavatazelva <72494135+glavatazelva@users.noreply.github.com>
2024-11-15 00:03:16 +0100

    nemam pojma

---
+++ Struggler/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System;
+using TMPro;
+public class HealthSystem : MonoBehaviour
+{
+    
+    private int numOfHearts = 2;
+    private Vector2 canvasPosition;
+
+
+    void Start()
+    {
+        canvasPosition = transform.position;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    
+}
