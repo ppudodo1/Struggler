709c555fcfaf5324c4ed2d05fafb53ca558e8dbe
glavatazelva <72494135+glavatazelva@users.noreply.github.com>
2024-11-28 21:38:59 +0100

    Hit Update
    Player can now get hit

--- Struggler/Assets/Scripts/PlayerMovement.cs
+++ Struggler/Assets/Scripts/PlayerMovement.cs
@@ -1,28 +1,46 @@
 using UnityEngine;
 using System;
+using System.Collections;
 public class PlayerMovement:MonoBehaviour
 {
     private float horizontal;
     private float speed = 5f;
+
     public float jumpingPower = 4f;
+    public float pushBackForce = -5f;
+
     private bool isFacingRight = true;
     private bool hasSecondJump = true;
+
+    //ima neki overlap sa fixed update pa ga nebi pravilno "gurnulo"
+    private bool isBeingPushed = false;
+
+    private SpriteRenderer spriteRenderer;
+
+    private Color defaultColor;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+
+    [SerializeField] private AudioClip hurtSound;
     [SerializeField] private AudioClip jumpSound;
-     [SerializeField] private AudioClip landingSound;
+    [SerializeField] private AudioClip landingSound;
+
     private AudioSource audioSource;
    
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        defaultColor = spriteRenderer.color;
     }
      void Update()
     {
         
+       
         horizontal = Input.GetAxisRaw("Horizontal");
+        
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
@@ -31,6 +49,7 @@ public class PlayerMovement:MonoBehaviour
             gameObject.GetComponent<Animator>().SetBool("isJumping", true);
 
         }
+
         //double jump
         else if(Input.GetButtonDown("Jump") && !(IsGrounded()) && hasSecondJump){
 
@@ -52,9 +71,9 @@ public class PlayerMovement:MonoBehaviour
     }
 
 
-    private void FixedUpdate()
-    {
-        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+    private void FixedUpdate(){
+        if(!isBeingPushed)
+            rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
         gameObject.GetComponent<Animator>().SetFloat("yVelocity", rb.linearVelocity.y);
     }
     private void Flip()
@@ -68,13 +87,56 @@ public class PlayerMovement:MonoBehaviour
     }
 
     private void OnTriggerEnter2D(Collider2D collision){
-        if (collision.gameObject.CompareTag("Ground"))
-        { 
+        if (collision.gameObject.CompareTag("Ground")){ 
             audioSource.PlayOneShot(landingSound);
             hasSecondJump = true;
             gameObject.GetComponent<Animator>().SetBool("isJumping", false);
         }
+
+
+        else if(collision.gameObject.CompareTag("Enemy")){
+            
+            if (collision.gameObject.transform.position.x > transform.position.x){
+                StartCoroutine(PushBack(false));
+            }
+
+            else{
+            StartCoroutine(PushBack(true)); 
+            }
+            
+        }
    
+    //kada primi damage
+    IEnumerator PushBack(bool pushRight){
+        isBeingPushed = true; 
+        Color currentColor = spriteRenderer.color;
+
+
+        if(defaultColor == spriteRenderer.color){
+            spriteRenderer.color = new Color(currentColor.r + 0.5f, currentColor.g * 0.7f,currentColor.b * 0.7f);
+
+            // da ne playa vise hurt zvukova u isto vrime
+            audioSource.PlayOneShot(hurtSound);
+        }
+
+        gameObject.GetComponent<Animator>().SetBool("isJumping", true);
+        if (!pushRight){
+
+            rb.linearVelocity = new Vector2(pushBackForce, jumpingPower * 0.7f);
+
+        }
+
+        else{
+
+            rb.linearVelocity = new Vector2(-pushBackForce, jumpingPower * 0.7f);
+
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        spriteRenderer.color = defaultColor;
+        isBeingPushed = false; 
+    }
 
     }
 }
