9a9e0141b4aa8660ffeeeabf57c330b541ca04e8
glavatazelva <72494135+glavatazelva@users.noreply.github.com>
2024-12-02 15:02:10 +0100

    Timeshifted ending timeline + logical error fix on timeline code

--- Struggler/Assets/Scripts/SkipMonologue.cs
+++ Struggler/Assets/Scripts/SkipMonologue.cs
@@ -7,10 +7,10 @@ public class EndingSceneTimelineBehaviour : MonoBehaviour{
 private float defaultTimerValue;
 public float timer = 2f;  
 
+private bool skipActivated = false;
+public float skipToTime = 112.5f;
 public PlayableDirector timeline;
-
 public ParticleSystem fairyDust;
-
 public GameObject skipText;
 
     void Start(){
@@ -25,18 +25,23 @@ public GameObject skipText;
             timer = defaultTimerValue;
         }
 
-        if (timer <= 0){
-            timeline.time = 112.5f;
+        if (timer <= 0 && !skipActivated){
+            timeline.time = skipToTime;
+            skipActivated = true;
+
             Destroy(skipText);
         }
         
-        if (timeline.time > 112.5f){
+        if (timeline.time > skipToTime){
+
+            skipActivated = true;
             fairyDust.Stop();
             Destroy(skipText);
 
         }
     }
 
+ 
     
 
 }
