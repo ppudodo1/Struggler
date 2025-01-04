206b2009ab3e3f654b028ea3a0d652081b49ee4e
glavatazelva <72494135+glavatazelva@users.noreply.github.com>
2025-01-04 02:38:03 +0100

    Leaf&Snow particle system fixed
    They now work as intented

--- Struggler/Assets/Scripts/PlayerScripts/FollowPlayerX.cs
+++ Struggler/Assets/Scripts/PlayerScripts/FollowPlayerX.cs
@@ -3,13 +3,10 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public Transform player; 
-    public float followSpeed = 2.0f; 
-    public float yPos = -1.5f;
-    void Update()
-    {
+    void Update(){
         float targetX = player.transform.position.x;
        
    
-        transform.position = new Vector2(targetX, yPos);
+        transform.position = new Vector2(targetX, transform.position.y);
     }
 }
