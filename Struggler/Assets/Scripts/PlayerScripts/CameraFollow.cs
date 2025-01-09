cf26e9618c9511db8f6e43b77d9af8f0bf33be3d
glavatazelva <72494135+glavatazelva@users.noreply.github.com>
2025-01-10 00:18:30 +0100

    Working on barriers

--- Struggler/Assets/Scripts/PlayerScripts/CameraFollow.cs
+++ Struggler/Assets/Scripts/PlayerScripts/CameraFollow.cs
@@ -3,15 +3,32 @@ using UnityEngine;
 public class CameraFollow:MonoBehaviour
 {
     public Transform player;
+    private bool touchedBarrier = false;
     public Vector3 offset;
     
      void Update()
     {
-        // transform.position = new Vector3(player.position.x + offset.x,player.position.y - 0.5f + offset.y, offset.z);
+        if(!touchedBarrier){
         transform.position = new Vector3(
              player.position.x + offset.x, 
              player.position.y + offset.y, 
              offset.z                     
          );
+        }
+
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag("Barrier")){
+            touchedBarrier = true;
+            transform.position = new Vector2 (other.transform.position.x, player.position.y + offset.y);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other){
+        if(other.CompareTag("Barrier")){
+            touchedBarrier = false;
+        }
     }
 }
