7a2e7fd9f412ec198d04a82f4d230535942e0f94
glavatazelva <72494135+glavatazelva@users.noreply.github.com>
2025-01-09 18:54:05 +0100

    A

--- Struggler/Assets/Scripts/MainMenu/PauseMenu.cs
+++ Struggler/Assets/Scripts/MainMenu/PauseMenu.cs
@@ -4,20 +4,21 @@ public class PauseMenu : MonoBehaviour
 {
 
     private bool pauseActive = false;
+    public GameObject pauseMenu;
     void Start(){
         gameObject.SetActive(false);
     }
 
     void Update(){
-
+        
 
         if(Input.GetKeyDown(KeyCode.Escape) &&  !pauseActive){
-            gameObject.SetActive(true);
+            pauseMenu.SetActive(true);
             pauseActive = true;
-            Time.timeScale = 0;
+            Time.timeScale = 0f;
         }
         else if(Input.GetKeyDown(KeyCode.Escape) && pauseActive){
-            gameObject.SetActive(false);
+            pauseMenu.SetActive(false);
             pauseActive = false;
             Time.timeScale = 1f;
         }
