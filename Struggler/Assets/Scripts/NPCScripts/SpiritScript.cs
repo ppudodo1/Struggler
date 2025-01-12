e85f4f07308dc6fca51b14491076bcd0f89ba578
ppudodo <dkatavic03@gmail.com>
2025-01-12 14:40:07 +0100

    test

--- Struggler/Assets/Scripts/NPCScripts/SpiritScript.cs
+++ Struggler/Assets/Scripts/NPCScripts/SpiritScript.cs
@@ -1,23 +1,28 @@
 using UnityEngine;
-
+using System.Collections;
 public class SpiritScript : MonoBehaviour
 {
     public Transform posA, posB;
     public int speed;
     Vector2 targetPos;
-    private bool isFacingRight = true;
+    //private bool isFacingRight = true;
+    private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    private bool isGrounded => Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
     void Start()
     {
         targetPos = posB.position;
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    
+
     void Update()
     {
         if (Vector2.Distance(transform.position, posA.position) < .1f)
         {
             targetPos = posB.position;
-            if (transform.localScale.x<0f) {
+            if (transform.localScale.x < 0f) {
                 Vector3 localScale = transform.localScale;
                 localScale.x *= -1f;
                 transform.localScale = localScale;
@@ -32,5 +37,23 @@ public class SpiritScript : MonoBehaviour
             transform.localScale = localScale;
         }
         transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        if (isGrounded)
+        {
+            StartCoroutine(DelayedJump());
+        }
+
+        //rb.linearVelocity = new Vector2(rb.linearVelocity.x, 5f);
     }
+    private IEnumerator DelayedJump() {
+        yield return new WaitForSeconds(0.2f);
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, 5f);
+    }
+   /*private void OnDrawGizmos()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, 0.1f);
+        }
+    }*/
 }
