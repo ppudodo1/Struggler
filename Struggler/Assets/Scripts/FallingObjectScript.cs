5ee11607e80c634f3f86d1e4b63ab0a0523e88ae
ppudodo <dkatavic03@gmail.com>
2025-02-02 09:21:20 +0100

    Toby be a good person

--- Struggler/Assets/Scripts/FallingObjectScript.cs
+++ Struggler/Assets/Scripts/FallingObjectScript.cs
@@ -3,33 +3,52 @@ using UnityEngine;
 public class FallingObjectScript : MonoBehaviour
 {
     private Rigidbody2D rb;
+    BoxCollider2D boxCollider2D;
+    public float distance;
     public int gravtiyMulitiplier = 1;
+    bool isFalling = false;
     void Start()
     {
         // Get the Rigidbody component
         rb = GetComponent<Rigidbody2D>();
-
-        // Disable gravity at the start
-        if (rb != null)
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        
+    }
+    private void Update()
+    {
+        Physics2D.queriesStartInColliders = false;
+        if (isFalling == false)
         {
-           // rb.useGravity = false;
+            RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, Vector2.down, distance);
+            Debug.DrawRay(transform.position, Vector2.down * distance, Color.red);
+
+            foreach (RaycastHit2D hit in hits)
+            {
+                if (hit.transform.CompareTag("Player"))
+                {  
+                    rb.gravityScale = 5;
+                    isFalling = true;
+                    break; 
+                }
+            }
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        // Check if the player triggered the collider
-        //Debug.Log("Collision");
-        if (other.CompareTag("Player"))
-        {
-            rb.gravityScale = gravtiyMulitiplier;
-            // Enable gravity when triggered
-            //Vector3 customGravity = Physics.gravity * 1.0f;
-            //rb.AddForce(customGravity, ForceMode.Acceleration);
-        }
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        //GetComponent<>
-    }
+
+    /* private void OnTriggerEnter2D(Collider2D other)
+     {
+         // Check if the player triggered the collider
+         //Debug.Log("Collision");
+         if (other.CompareTag("Player"))
+         {
+             rb.gravityScale = gravtiyMulitiplier;
+             // Enable gravity when triggered
+             //Vector3 customGravity = Physics.gravity * 1.0f;
+             //rb.AddForce(customGravity, ForceMode.Acceleration);
+         }
+     }
+     private void OnCollisionEnter2D(Collision2D collision)
+     {
+         //GetComponent<>
+     }*/
 }
